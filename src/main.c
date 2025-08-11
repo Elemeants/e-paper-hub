1c44f7e931970b2809ec4db512e93097c1c094d2
Elemeants <jdanypa@gmail.com>
2025-08-10 18:58:32 -0600

    Add firmware version flag
    Signed-off-by: Elemeants <jdanypa@gmail.com>

--- src/main.c
+++ src/main.c
@@ -29,7 +29,21 @@ static max17048_i2c_driver_config_t MAX17048_CONFIG = {
   .scl_pin = GPIO_NUM_13,
 };
 
+static char *LOGO =
+"  _____                      _    _       _     \n"
+"  |  __ \\                    | |  | |     | |    \n"
+"  | |__) |_ _ _ __   ___ _ __| |__| |_   _| |__  \n"
+"  |  ___/ _` | '_ \\ / _ \\ '__|  __  | | | | '_ \\ \n"
+"  | |  | (_| | |_) |  __/ |  | |  | | |_| | |_) |\n"
+"  |_|   \\__,_| .__/ \\___|_|  |_|  |_|\\__,_|_.__/ \n"
+"              | |                                \n"
+"              |_|                                \n";
+
 void app_main() {
+  printf(LOGO);
+  printf(" Firmware Version: %s\n", FIRMWARE_VERSION);
+  printf(" Build time: %u\n", BUILD_TIME_UNIX);
+
   ws42_driver_init(SCREEN_CONFIG);
   max17048_i2c_driver_init(MAX17048_CONFIG);
 
